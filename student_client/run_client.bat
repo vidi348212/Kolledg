3792bc74eb01aac09d3d8c64afaf5ffff0fba1b9
vidi348212 <vidi348212@gmail.com>
2026-09-07 20:55:02 +0300

    Delete student_client directory

--- student_client/run_client.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-REM Запуск клиента ученика в фоновом режиме
-REM Для автозагрузки добавьте этот файл в автозагрузку Windows
-
-cd /d "%~dp0"
-start "" python student_client\client.py --auto
-echo Клиент запущен
